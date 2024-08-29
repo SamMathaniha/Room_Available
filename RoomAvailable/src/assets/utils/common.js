d798de42421c8ada4ca4593c06855bfa9587e4ae
SamMathaniha <mathanihasam@gmail.com>
2024-08-29 18:08:41 +0530

    using swiper developes rooms

---
+++ RoomAvailable/src/assets/utils/common.js
@@ -0,0 +1,22 @@
+export const sliderSettings = {
+  slidesPerView: 1,
+  spaceBetween: 40,
+  breakpoints: {
+    // Small screens
+    480: {
+      slidesPerView: 1,
+    },
+    // Medium screens
+    600: {
+      slidesPerView: 2,
+    },
+    // Larger screens
+    700: {
+      slidesPerView: 3,
+    },
+    // Extra-large screens
+    1100: {
+      slidesPerView: 4,
+    },
+  },
+};
--- RoomAvailable/src/componenets/Rooms/Rooms.jsx
+++ RoomAvailable/src/componenets/Rooms/Rooms.jsx
@@ -1,8 +1,43 @@
 import React from "react";
 import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import "swiper/css";
+import "./Rooms.css";
+import data from "../../assets/utils/slider.json";
+import { sliderSettings } from "../../assets/utils/common";
 
 const Rooms = () => {
-  return <div>Rooms</div>;
+  return (
+    <section className="Room-Wrapper">
+      <div className="Room-Container">
+        <div className="room-head">
+          <span>Best Choices</span>
+          <span>Popular Rooms</span>
+        </div>
+
+        <Swiper {...sliderSettings}>
+          {data.map((card, i) => (
+            <SwiperSlide key={i}>
+              <div className="Room-card">
+                <img src={card.image} alt="HomeImg" />
+
+                <span className="secondaryText r-Price">
+                  <br></br>
+                  <span style={{ color: "orange" }}>$</span>
+                  <span>{card.price}</span>
+                </span>
+                <br></br>
+                <span className="primaryText">{card.name}</span>
+                <br></br>
+                <span className="secondaryText">{card.detail}</span>
+                <br></br>
+                <span className="secondaryText">For Days: {card.Days}</span>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
+  );
 };
 
 export default Rooms;
