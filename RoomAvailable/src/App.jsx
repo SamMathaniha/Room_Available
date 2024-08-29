7a78fbef06810b2f6ff875b6070080190cf3b4a6
SamMathaniha <mathanihasam@gmail.com>
2024-08-29 16:47:40 +0530

    Hotel section developed

--- RoomAvailable/src/App.jsx
+++ RoomAvailable/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./componenets/Header/Header";
 import Hero from "./componenets/Hero/Hero";
+import Hotels from "./componenets/Hotels/Hotels";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
+      <Hotels />
     </div>
   );
 }
---
+++ RoomAvailable/src/componenets/Hotels/Hotels.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Hotels.css";
+
+const Hotels = () => {
+  return (
+    <div className="Hotel-wrapper">
+      <div className="Hotel-container">
+        <img src="./1 HotelLogo.png" alt="HotelLogo1" />
+        <img src="./2 HotelLogo.png" alt="HotelLogo2" />
+        <img src="./3 HotelLogo.png" alt="HotelLogo3" />
+        <img src="./4 HotelLogo.png" alt="HotelLogo4" />
+        <img src="./5 HotelLogo.png" alt="HotelLogo5" />
+        <img src="./6 HotelLogo.png" alt="HotelLogo6" />
+      </div>
+    </div>
+  );
+};
+
+export default Hotels;
