0f1e74d7cb631c023f5275920a068896039cb246
SamMathaniha <mathanihasam@gmail.com>
2024-08-29 23:40:33 +0530

    Added motion effect to H1

--- RoomAvailable/src/componenets/Hero/Hero.jsx
+++ RoomAvailable/src/componenets/Hero/Hero.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import "./Hero.css";
-
+import { motion } from "framer-motion";
 const Hero = () => {
   return (
     <section className="wrapper">
       <div className="container">
         <div className="ContainerleftSide">
-          <h1 className="HeroHeader">Discover Most suitable Room</h1>
+          <motion.h1
+            className="HeroHeader"
+            initial={{ y: "2rem", opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              duration: 5,
+              type: "spring",
+            }}
+          >
+            Discover Most suitable Room
+          </motion.h1>
           <h2 className="HeroDescription">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry.
